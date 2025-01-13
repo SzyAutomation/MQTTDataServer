704aa0e54ddce1e3e3f4a2b50155d86ebcdca08e
Nathan Szymesko <nathanszymesko@szyauto.com>
2025-01-13 17:22:36 -0600

    add in production variables

---
+++ config/prod.js
@@ -0,0 +1,10 @@
+module.exports = {
+    dBHost: process.env.DB_HOST,
+    dBUser: process.env.BD_USER,
+    dBPassword: process.env.DB_PASSWORD,
+    dBName: process.env.DB_NAME,
+    mqttHost: process.env.MQTT_HOST,
+    mqttPort: process.env.MQTT_PORT,
+    mqttUsername: process.env.MQTT_USERNAME,
+    mqttPassword: process.env.MQTT_PASSWORD,
+}
